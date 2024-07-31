7147a62ef4286bd39e01d71da5bc4c001b4b4c47
Roland König <roland.koenig@rolandk.de>
2024-07-31 22:01:26 +0200

    Updted topic-store to use shallowRef

--- src/RKTimeTrack.Service/ClientApp/src/stores/topic-store.ts
+++ src/RKTimeTrack.Service/ClientApp/src/stores/topic-store.ts
@@ -1,10 +1,10 @@
 ﻿import {defineStore} from "pinia";
-import {inject, ref, type Ref} from "vue";
+import {inject, shallowRef, type Ref} from "vue";
 import {TimeTrackClient, TimeTrackingTopic} from "@/services/time-track-client.generated";
 
 export const useTopicStore = defineStore('topicStore', () => {
     const timeTrackClient = inject<TimeTrackClient>("TimeTrackClient")!;
-    const topics: Ref<TimeTrackingTopic[]> = ref([]);
+    const topics: Ref<TimeTrackingTopic[]> = shallowRef([]);
 
     async function fetchInitialData() {
         topics.value = await timeTrackClient.getAllTopics();
