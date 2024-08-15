f1333a73de8070c83f491a176292a16d7fcb1fe2
Roland König <roland.koenig@rolandk.de>
2024-08-15 15:06:08 +0200

    Updated publish script

--- scripts/docker-build-and-push.ps1
+++ scripts/docker-build-and-push.ps1
@@ -14,14 +14,12 @@ if(-Not $DockerHubPassword){
 }
 
 # Get infos from git
-$commitHash = (git rev-parse HEAD).Substring(0,16)
 $branchName = git rev-parse --abbrev-ref HEAD
 
 # Docker build-and-push
 docker login -u $DockerHubUser -p $DockerHubPassword
 docker build -t rolandk87/rktimetrack:latest `
              -t rolandk87/rktimetrack:latest-$branchName `
-             -t rolandk87/rktimetrack:$branchName-$commitHash `
              --platform linux/amd64 `
              -f ../src/RKTimeTrack.Service/Dockerfile ../
 docker push -a rolandk87/rktimetrack
