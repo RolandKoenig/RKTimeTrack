512cc4aa4f655f904abd39141dec296a84f46438
Roland König <roland.koenig@rolandk.de>
2025-01-27 11:19:02 +0800

    Refactoring tests

--- src/RKTimeTrack.Service.IntegrationTests/UiTests/StartupTests.cs
+++ src/RKTimeTrack.Service.IntegrationTests/UiTests/StartupTests.cs
@@ -4,17 +4,17 @@ using RKTimeTrack.Service.IntegrationTests.Util;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace RKTimeTrack.Service.IntegrationTests;
+namespace RKTimeTrack.Service.IntegrationTests.UiTests;
 
 [Collection(nameof(TestEnvironmentCollection))]
-public class UiTests
+public class StartupTests
 {
     private const bool HEADLESS_MODE = true;
     private static readonly float? SLOW_MODE_MILLISECONDS = null;
     
     private readonly WebHostServerFixture _server;
     
-    public UiTests(
+    public StartupTests(
         WebHostServerFixture server,
         ITestOutputHelper testOutputHelper)
     {
@@ -40,7 +40,7 @@ public class UiTests
             SlowMo = SLOW_MODE_MILLISECONDS
         });
         var page = await browser.NewPageAsync();
-
+        
         // Act
         await page.GotoAsync(rootUri.AbsoluteUri);
 
