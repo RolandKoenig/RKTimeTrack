cbed30b16f0fcf4a126fff2b7548dd059d32a909
Roland König <roland.koenig@rolandk.de>
2025-01-28 10:28:07 +0800

    Refactoring for ui tests

---
+++ src/RKTimeTrack.Service.IntegrationTests/TestSettings.cs
@@ -0,0 +1,7 @@
+namespace RKTimeTrack.Service.IntegrationTests;
+
+internal static class TestSettings
+{
+    public const bool HEADLESS_MODE = true;
+    public static readonly float? SLOW_MODE_MILLISECONDS = null;
+}
