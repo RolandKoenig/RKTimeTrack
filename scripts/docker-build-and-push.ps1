3fb5123543e71606ca01ac28bacd7a801b37dfb3
Roland König <roland.koenig@rolandk.de>
2026-06-19 14:33:20 +0200

    Added commit hash as a tag on the docker image

--- scripts/docker-build-and-push.ps1
+++ scripts/docker-build-and-push.ps1
@@ -10,11 +10,13 @@ $ErrorActionPreference = "Stop"
 
 # Get infos from git
 $branchName = git rev-parse --abbrev-ref HEAD
+$commitSha = git rev-parse --short HEAD
 
 # Docker build-and-push
 docker login -u $DockerHubUser -p $DockerHubPassword
 docker build -t docker.io/rolandk87/rktimetrack:latest-$branchName `
              -t docker.io/rolandk87/rktimetrack:latest `
+             -t docker.io/rolandk87/rktimetrack:$commitSha `
              --platform linux/amd64 `
              -f ../src/RolandK.TimeTrack.Service/Dockerfile ../
 docker push -a docker.io/rolandk87/rktimetrack
