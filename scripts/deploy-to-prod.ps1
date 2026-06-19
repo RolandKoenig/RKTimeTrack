601f48584a8f1efaf00439e4f532caf388dcd810
Roland König <roland.koenig@rolandk.de>
2026-06-19 15:12:36 +0200

    Updated ci/cd pipeline

---
+++ scripts/deploy-to-prod.ps1
@@ -0,0 +1,27 @@
+Param(
+    [Parameter(Mandatory = $true)]
+    [string]$DockerHubUser,
+
+    [Parameter(Mandatory = $true)]
+    [string]$DockerHubPassword
+)
+
+# Get infos from git
+$commitSha = git rev-parse --short HEAD
+$branchName = git rev-parse --abbrev-ref HEAD
+
+# Check whether the image exists
+skopeo login --username $DockerHubUser --password $DockerHubPassword registry.hub.docker.com
+if($LastExitCode){
+    throw "Unable to login to dockerhub!"
+}
+
+skopeo inspect docker://registry.hub.docker.com/rolandk87/rktimetrack:$commitSha
+if($LastExitCode){
+    throw "The image rolandk87/dcn-website:$commitSha does not exist!"
+}
+
+# Push to Azure Container Apps
+az containerapp update -n rolandk-timetrack-prod-app `
+                       --resource-group RolandK-TimeTrack-PROD `
+                       --image registry.hub.docker.com/rolandk87/rktimetrack:$commitSha
